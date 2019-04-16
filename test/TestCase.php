ddbf4d240cefb3d930f62a7f2de16b8e366857fe
Vuong Minh <vuongxuongminh@gmail.com>
2019-04-17 00:08:42 +0700

    Fixed app test vendor path [skip ci]

--- test/TestCase.php
+++ test/TestCase.php
@@ -49,7 +49,7 @@ class TestCase extends BaseTestCase
             'id' => 'test',
             'basePath' => __DIR__,
             'bootstrap' => ['vxm\desktopNotifier\Bootstrap'],
-            'vendorPath' => dirname(__DIR__, 2) . '/vendor'
+            'vendorPath' => dirname(__DIR__) . '/vendor'
         ], $config));
     }
 
